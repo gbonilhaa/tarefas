99f0fafbc0d4fd6b09e6e8f03efa3189e1c1d517
vagrant <vagrant@precise32.(none)>
2014-03-17 14:59:19 +0000

    adicinou listar usuarios

--- app/controllers/usuarios_controler.rb
+++ app/controllers/usuarios_controler.rb
@@ -1,4 +1,9 @@
 class UsuariosController < ApplicationController
+  def index
+ 	 @usuario = Usuario.all? 
+ 	 render :index
+  end
+
   def create
     Usuario.create nome: params[:nome], email: params[:email], password_digest: params[:password_digest]
     redirect_to :back
@@ -7,5 +12,11 @@ class UsuariosController < ApplicationController
   def update
    Tarefa.update nome: params[:nome], password_digest: params[:password_digest]
     redirect_to tarefas_path
+  end
+
+  def destroy
+    usuario = Usuario.find params[:id]
+    usuario.destroy
+    redirect_to usuarios_path
   end
 end
